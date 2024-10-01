1525fe5bd84f472a5d52d2670cae06c79801812a
Nathan Wise <nathanwise@gmail.com>
2024-10-01 17:48:55 -0400

    Initial commit

---
+++ lib/altered_access.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "altered_access/version"
+
+module AlteredAccess
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/altered_access_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe AlteredAccess do
+  it "has a version number" do
+    expect(AlteredAccess::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
